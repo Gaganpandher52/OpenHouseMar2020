9ce0a321913d749361a3f6080b21fafe13c59877
gaganpandher52 <gaganpandher52@gmail.com>
2020-03-06 14:56:18 -0700

    created new components

---
+++ src/components/communities/CommunityDisplay.tsx
@@ -0,0 +1,23 @@
+// import React, { Component } from 'react';
+import * as React from "react";
+
+class CommunityDisplay extends React.Component<{}, any> {
+
+  constructor(props:{}){
+    super(props);
+    this.state = {
+
+    }
+  }
+
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default CommunityDisplay;
