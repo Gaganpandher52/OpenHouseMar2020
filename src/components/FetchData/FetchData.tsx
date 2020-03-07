7460ef2b56256c207980d05957272acbc6199055
gaganpandher52 <gaganpandher52@gmail.com>
2020-03-07 09:46:27 -0700

    added fetch function to retrive data from api call

--- src/components/FetchData/FetchData.tsx
+++ src/components/FetchData/FetchData.tsx
@@ -8,12 +8,61 @@ class FetchData extends React.Component<{}, any> {
       communityNames: [],
       communityPrices: [],
       errors: null,
-      isLoaded: false
+      loadInfo: false
     }
   }
 
   componentDidMount(){
-    
+    const requestedData = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+
+    this.CommunityDataFetch({requestedData: requestedData})
+    this.PriceDataFetch({requestedData: requestedData})
+
+  }
+
+  CommunityDataFetch = ({requestedData}:{requestedData:any}) =>{
+    fetch("https://a18fda49-215e-47d1-9dc6-c6136a04a33a.mock.pstmn.io/communities", requestedData)
+      .then(response => {
+        this.setState({
+        });
+        return response.text();
+      })
+      .then((result) => {
+        this.setState({
+          loadInfo: true,
+          communityNames: JSON.parse(result)
+        });
+      })
+      .catch(error => {
+        console.log('error', error);
+        this.setState({
+          error: true
+        })
+      });
+  }
+
+  PriceDataFetch = ({requestedData}:{requestedData:any}) =>{
+    fetch("https://a18fda49-215e-47d1-9dc6-c6136a04a33a.mock.pstmn.io/homes", requestedData)
+      .then(response => {
+        this.setState({
+        });
+        return response.text();
+      })
+      .then((result) => {
+        this.setState({
+          loadInfo: true,
+          communityPrices: JSON.parse(result)
+        });
+      })
+      .catch(error => {
+        console.log('error', error);
+        this.setState({
+          error: true
+        })
+      });
   }
 
 
@@ -21,6 +70,7 @@ class FetchData extends React.Component<{}, any> {
   render() {
     return (
       <div>
+        <CommunityDisplay />
         
       </div>
     );
